c24bdd76457086e106ec8b8769861b04aed0dcb1
Joe Huss <detain@interserver.net>
2018-01-04 15:44:57 -0500

    removing 2 functionms not in use

--- src/Plugin.php
+++ src/Plugin.php
@@ -61,37 +61,6 @@ class Plugin {
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getMenu(GenericEvent $event) {
-		$menu = $event->getSubject();
-		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link(self::$module, 'choice=none.reusable_xen', 'images/icons/database_warning_48.png', 'ReUsable Xen Licenses');
-			$menu->add_link(self::$module, 'choice=none.xen_list', 'images/icons/database_warning_48.png', 'Xen Licenses Breakdown');
-			$menu->add_link(self::$module.'api', 'choice=none.xen_licenses_list', '/images/whm/createacct.gif', 'List all Xen Licenses');
-		}
-	}
-
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event) {
-		$loader = $event->getSubject();
-		$loader->add_page_requirement('crud_xen_list', '/../vendor/detain/crud/src/crud/crud_xen_list.php');
-		$loader->add_page_requirement('crud_reusable_xen', '/../vendor/detain/crud/src/crud/crud_reusable_xen.php');
-		$loader->add_requirement('get_xen_licenses', '/../vendor/detain/myadmin-xen-vps/src/xen.inc.php');
-		$loader->add_requirement('get_xen_list', '/../vendor/detain/myadmin-xen-vps/src/xen.inc.php');
-		$loader->add_page_requirement('xen_licenses_list', '/../vendor/detain/myadmin-xen-vps/src/xen_licenses_list.php');
-		$loader->add_page_requirement('xen_list', '/../vendor/detain/myadmin-xen-vps/src/xen_list.php');
-		$loader->add_requirement('get_available_xen', '/../vendor/detain/myadmin-xen-vps/src/xen.inc.php');
-		$loader->add_requirement('activate_xen', '/../vendor/detain/myadmin-xen-vps/src/xen.inc.php');
-		$loader->add_requirement('get_reusable_xen', '/../vendor/detain/myadmin-xen-vps/src/xen.inc.php');
-		$loader->add_page_requirement('reusable_xen', '/../vendor/detain/myadmin-xen-vps/src/reusable_xen.php');
-		$loader->add_requirement('class.Xen', '/../vendor/detain/xen-vps/src/Xen.php');
-		$loader->add_page_requirement('vps_add_xen', '/vps/addons/vps_add_xen.php');
-	}
-
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
 		$settings->add_text_setting(self::$module, 'Slice Costs', 'vps_slice_xen_cost', 'XEN VPS Cost Per Slice:', 'XEN VPS will cost this much for 1 slice.', $settings->get_setting('VPS_SLICE_XEN_COST'));
